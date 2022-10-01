86b8574913b532039986756125ce728f2aada6f5
Igor Kompanets <kombainer86@gmail.com>
2022-10-01 19:22:53 +0300

    Task5 done

---
+++ Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать все четные числа в интервале от 1 до N
+
+Console.WriteLine("Эта программа выведет все четные числа в диапазоне от 1 до N. Введите значение N - конец диапазона");
+int N = int.Parse(Console.ReadLine());
+int first = 1;
+int size = N - 1;
+int [] arr = new int[size];
+int i = 0;
+while(first <= size)
+{
+    arr[i] = first;
+    first++;
+    i++;
+    }   
+i=0;
+while(i<size)
+{
+    if(arr[i] % 2== 0)
+    {
+        Console.WriteLine(arr[i]);
+    }
+    i++;
+}
