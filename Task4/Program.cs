7c186ede9551a7241b663b1ff980fc0c408783f0
Igor Kompanets <kombainer86@gmail.com>
2022-10-01 14:52:16 +0300

    Task4 done

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Проверить, является ли число четным. Пользователь будет сам вводить число
+
+Console.WriteLine("Ведите любое число");
+int a = int.Parse(Console.ReadLine());
+
+    if(a%2==0)
+    {
+        Console.WriteLine("Число четное");
+    }
+    else
+    {
+        Console.WriteLine("Число нечетное");
+    }
